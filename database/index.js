da2d3fdebd0f4ab3685a5b62f2c8adec81631045
Oscar Henrique Lucas Henrichs <oscar.henrichs@outlook.com>
2020-10-10 12:35:13 -0300

    commit start backend 4 - migrations

--- database/index.js
+++ database/index.js
@@ -1,9 +1,19 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, ConnectionError } = require('sequelize');
+const dbConfig = require("../config/database.js");
+const { connect } = require('../routes/transactionsUser.js');
 
 
+const connection = new Sequelize(dbConfig);
 
-// Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+run().catch(error => console.log(error.stack));
+
+// async function run() {
+// try {
+//      await connection.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
+
+module.exports = connection;
