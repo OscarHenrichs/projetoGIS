d053ef2352307b8551d560c608d24d2792df1223
Oscar Henrique Lucas Henrichs <oscar.henrichs@outlook.com>
2020-10-10 10:12:35 -0300

    commit2

---
+++ src/components/transactionList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const transactionList = () => {
+    return (
+        <div>
+            <h3>Lista de Usuários</h3>
+            <ul id="list" class="list">
+                <li class="minus">
+                    <span>Nome: Fulano - Email: Fulano@email</span>
+                </li>
+            </ul>
+        </div>
+    )
+}
